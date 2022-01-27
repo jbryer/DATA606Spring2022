7990340c5980d9bf78b8640d1a279b23d96a39dc
Jason Bryer <jason@bryer.org>
2022-01-27 15:42:10 -0500

    Added intro to course slides.

--- build.R
+++ build.R
@@ -5,8 +5,10 @@
 # Builds the course website.
 
 library(xaringanBuilder)
+library(readxl)
 
-# First copy the Slides and supplemental materials to the docs/folder
+
+# Copy the Slides and supplemental materials to the docs/folder
 tocopy <- c(list.files('Slides', pattern = '.html'),
 			list.dirs('Slides', recursive = FALSE, full.names = FALSE))
 for(i in tocopy) {
@@ -31,7 +33,7 @@ for(i in tocopy) {
 	if(tolower(tools::file_ext(from)) == 'html') {
 		pdf <- paste0(tools::file_path_sans_ext(from), '.pdf')
 
-		build_pdf <- !file.exists(pdf) | file.info(from)$mtime >	file.info(pdf)$mtime
+		build_pdf <- !file.exists(pdf) | file.info(from)$mtime > file.info(pdf)$mtime
 
 		if(build_pdf) {
 			wd <- setwd('Slides/')
@@ -44,8 +46,48 @@ for(i in tocopy) {
 				print(e)
 			}, finally = { setwd(wd) })
 		}
+		
+		if(exisits(pdf)) { # Copy PDF to docs directory
+			file.copy(pdf, paste0('docs/slides/', basename(pdf)))
+		}
 	}
 }
+
+# Build blog posts for meetups
+meetups <- read_excel('Schedule.xlsx', sheet = 'Meetups')
+for(i in 1:nrow(meetups)) {
+	if(!is.na(meetups[i,]$Slides) | !is.na(meetups[i,]$Youtube)) {
+		blogfile <- paste0('website/content/blog/', as.Date(meetups[i,]$Date), '.Rmd')
+		blogpath <- paste0('/blog/', as.Date(meetups[i,]$Date), '/')
+		
+		blogcontent <- ''
+		if(!is.na(meetups[i,]$Slides)) {
+			blogcontent <- paste0(blogcontent, '[Click here](/slides/', meetups[i,]$Slides, '.html#1) to open the slides ([PDF](/slides/', meetups[i,]$Slides, '.pdf)).\n\n')
+			blogfile <- paste0('website/content/blog/', meetups[i,]$Slides, '.Rmd')
+		}
+		if(!is.na(meetups[i,]$Youtube)) {
+			blogcontent <- paste0(blogcontent, '<iframe width="560" height="315" src="https://www.youtube.com/embed/', meetups[i,]$Youtube, '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+		}
+		
+		pubdate <- as.character(min(as.Date(meetups[i,]$Date), Sys.Date()))
+		
+		cat('---', '\n',
+			'title: "', meetups[1,]$Topic, '"\n',
+			'author: "Jason Bryer"', '\n',
+			'date: ', pubdate, '\n',
+			'draft: false', '\n',
+			'categories: ["Meetups"]', '\n',
+			'tags: ["Annoucement"]', '\n',
+			'---', '\n', '\n',
+			blogcontent, '\n\n',
+			'<!-- more -->\n\n',
+			blogcontent, '\n\n',
+			sep  = '',
+			file = blogfile)
+	}
+}
+
+# Build the site with blogdown
 wd <- setwd('website')
 blogdown::build_site(build_rmd = TRUE)
 setwd(wd)
